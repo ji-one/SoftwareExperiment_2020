b83d0581847c6c67eb02479817b13a3cd3abf3aa
jiwon kim <rlawldnjs8@gmail.com>
2020-05-29 03:02:59 +0900

    Create Scoreboard.kt

---
+++ week10/Scoreboard.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+class Scoreboard {
+ var board = IntArray(20)
+ get() {
+ var x = 0
+ while (x < 20) {
+ if (field[x] > 15) {
+ field[x] = 15
+ } else {
+ field[x] = field[x]
+ }
+ x++
+ }
+ return field
+ }
+ var idx: Int = 0
+ var score: Int = 0
+ set(value) {
+ board[idx] = value
+ ++idx
+ }
+}
