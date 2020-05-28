c80d59be3dee253516e241a9b51b8fa7f3413484
jiwon kim <rlawldnjs8@gmail.com>
2020-05-29 03:02:20 +0900

    Create Main2Activity.kt

---
+++ week10/Main2Activity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.content_main2.*
+class Main2Activity : AppCompatActivity() {
+ override fun onCreate(savedInstanceState: Bundle?) {
+ super.onCreate(savedInstanceState)
+ setContentView(R.layout.activity_main2)
+ setSupportActionBar(toolbar)
+ fab.setOnClickListener { view ->
+ Snackbar.make(view, "Replace with your own action",
+Snackbar.LENGTH_LONG)
+ .setAction("Action", null).show()
+ }
+ var array = IntArray(20)
+ array =intent.getIntArrayExtra("points")
+ var arrayList = ArrayList<Int>(20)
+ for(index in array.indices){
+ arrayList.add(array[index])
+ }
+ listView.adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,
+arrayList)
+ }
+}
